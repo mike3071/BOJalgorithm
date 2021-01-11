7bc2a71775214410593eee6d583b7d20bd62162f
Doyoung Kim <70436657+mike3071@users.noreply.github.com>
2021-01-11 23:15:52 +0900

    Update BFS1707.py

--- BFS1707.py
+++ BFS1707.py
@@ -1,43 +1,38 @@
-'''
 from collections import deque
+import sys
+input = sys.stdin.readline
 def bfs(k):
+    color[k]=1
     queue = deque()
     queue.append(k)
-    color[k]=1
-    visited[k]=1
     while queue:
         out=queue.popleft()
         for i in graphshape[out]:
-            if visited[i]==0 and color[out]==1:
+            if color[i]==0:
+                color[i]= (- color[out])
                 queue.append(i)
-                color[i]=-1
-                visited[i]=1
-            if visited[i]==0 and color[out]==-1:
-                queue.append(i)
-                color[i]=1
-                visited[i]=1
-    for w in range(1,v+1):
-        for q in graphshape[w]:
-            if color[w]==color[q]:
-                return 'NO'
+            else:
+                if color[i]==color[out]:
+                    return 'NO'
     return 'YES'
+  
     
     
 num = int(input())
 for i in range(num):
     v, e= map(int,input().split())
     graphshape = [[] for _ in range(v+1)]
-    visited = [0]*(v+1)
     color = [0]*(v+1)
     for j in range(e):
         start, end = map(int, input().split())
         graphshape[start].append(end)
         graphshape[end].append(start)
-    decision = []
+    ans = 'YES'
     for r in range(1,v+1):
-        decision.append(bfs(r))
-    if 'YES' in decision:
-        print('YES')
-    else:
-        print('NO')
-'''
+        if color[r]==0:
+            p = bfs(r)
+            if p == 'NO':    
+                ans = 'NO'
+                break
+    print(ans)
+    
